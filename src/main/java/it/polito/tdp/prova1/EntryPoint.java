531a76c7d6223e8ca0703e066dc6dbd09e1aadbb
Maco <Maco@LAPTOP-O1JLL8OB>
2020-03-25 17:45:32 +0100

    Aggiunto un commento

--- src/main/java/it/polito/tdp/prova1/EntryPoint.java
+++ src/main/java/it/polito/tdp/prova1/EntryPoint.java
@@ -21,6 +21,8 @@ public class EntryPoint extends Application {
         stage.setScene(scene);
         stage.show();
     }
+    
+    //Commento di prova
 
     /**
      * The main() method is ignored in correctly deployed JavaFX application.
